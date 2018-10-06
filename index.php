e3ef720e5f037ee7f56d97a9a771f4825f6d63af
Ikkeh1001 <thijs.moen@gmail.com>
2018-10-06 21:49:16 +0200

    First progress

--- index.php
+++ index.php
@@ -7,27 +7,66 @@ require_once ("database.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Scicheck</title>
+  <meta charset="UTF-8">
+  <title>Scicheck</title>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <meta name="mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <meta name="mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
 
-    <link rel="manifest" href="manifest.json">
+  <link rel="manifest" href="manifest.json">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
 
-    <!--link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png"-->
+  <!--link rel="icon" type="image/png" sizes="96x96" href="img/favicon-96x96.png"-->
 
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/home.js"></script>
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/home.js"></script>
 
-    <script src="https://code.jquery.com/jquery-migrate-3.0.1.js"></script>
+  <script src="https://code.jquery.com/jquery-migrate-3.0.1.js"></script>
 </head>
 <body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand pl-2" href="index.php">Scicheck</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav ml-2 ml-md-5">
+        <li class="nav-item site-header px-md-3">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item site-header dropdown px-md-3">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <main role="main" class="d-flex flex-row justify-content-center pt-5">
+
+    <div class="shit pt-2">
+      <h1>Bootstrap starter template</h1>
+      <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <p>Ding</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </main>
 </body>
+
 </html>
